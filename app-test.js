5431854b8ac67bea4660c8c5983d84a5c0ff7e3d
Dexter <ambidextrous.in@gmail.com>
2026-08-20 14:03:51 +0300

    chanage the chai version to newwe one

--- app-test.js
+++ app-test.js
@@ -1,12 +1,10 @@
 let mongoose = require("mongoose");
 let server = require("./app");
 let chai = require("chai");
-// let chaiHttp = require("chai-http");
+const { request } = require("chai-http");
 
 // Assertion
 chai.should();
-// chai.use(chaiHttp);
-const chaiHttp = require("chai-http").default || require("chai-http");
 
 describe("Planets API Suite", () => {
   describe("Fetching Planet Details", () => {
@@ -14,12 +12,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 1,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(1);
           res.body.should.have.property("name").eql("Mercury");
           done();
@@ -30,12 +28,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 2,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(2);
           res.body.should.have.property("name").eql("Venus");
           done();
@@ -46,12 +44,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 3,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(3);
           res.body.should.have.property("name").eql("Earth");
           done();
@@ -61,12 +59,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 4,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(4);
           res.body.should.have.property("name").eql("Mars");
           done();
@@ -77,12 +75,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 5,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(5);
           res.body.should.have.property("name").eql("Jupiter");
           done();
@@ -93,12 +91,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 6,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(6);
           res.body.should.have.property("name").eql("Saturn");
           done();
@@ -109,12 +107,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 7,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(7);
           res.body.should.have.property("name").eql("Uranus");
           done();
@@ -125,12 +123,12 @@ describe("Planets API Suite", () => {
       let payload = {
         id: 8,
       };
-      chai
-        .request(server)
+      request
+        .execute(server)
         .post("/planet")
         .send(payload)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("id").eql(8);
           res.body.should.have.property("name").eql("Neptune");
           done();
@@ -158,11 +156,11 @@ describe("Planets API Suite", () => {
 describe("Testing Other Endpoints", () => {
   describe("it should fetch OS Details", () => {
     it("it should fetch OS details", (done) => {
-      chai
-        .request(server)
+      request
+        .execute(server)
         .get("/os")
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           done();
         });
     });
@@ -170,11 +168,11 @@ describe("Testing Other Endpoints", () => {
 
   describe("it should fetch Live Status", () => {
     it("it checks Liveness endpoint", (done) => {
-      chai
-        .request(server)
+      request
+        .execute(server)
         .get("/live")
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("status").eql("live");
           done();
         });
@@ -183,11 +181,11 @@ describe("Testing Other Endpoints", () => {
 
   describe("it should fetch Ready Status", () => {
     it("it checks Readiness endpoint", (done) => {
-      chai
-        .request(server)
+      request
+        .execute(server)
         .get("/ready")
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.property("status").eql(200);
           res.body.should.have.property("status").eql("ready");
           done();
         });
